dea401079e42c2cb87eda70db5dcf1ee6e57640a
Justin Jose <justin.jose@oracle.com>
2025-02-03 11:08:01 +0530

    Bug#37462058 GTID and binlog record exists but row is missing,             replica stops with error 1032
     Description:
     ------------
     In source-replica setup, replica is encountering random failures of
     UPDATE/DELETE statements with "HA_ERR_KEY_NOT_FOUND" error on
     the same table. The replica binlog and GTID records show that the row
     required was committed and has not been deleted/updated.
    
    
     Analysis:
     ------------
     The issue occurs in replica when slave_rows_search_algorithms is
     HASH_SCAN and two rows in a table have the same CRC32 value. When
     the replica processes an update event, it creates an entry in a
     multi-valued hash table for the event. This hash table maps each
     CRC32 value to a list of row updates whose CRC32 corresponds to
     the Before-Image.  At a high level, the logic for processing
     an update table row in `Rows_log_event::do_scan_and_update` is
     as follows:
      - If the same row was updated multiple times on the source, perform
        a chain of updates on that row.
      - In each step of the chain:
          - Look up the row's CRC32 in the hash table.
          - Iterate over multiple entries in the hash table with the same
            CRC32 until one matching the current row is found.
          - Apply that update to the current row.
          - Continue to the next step in the chain as long as the updated
            row’s hash is found in the hash table.
    
     Consider two rows, R1 and R2, that have the same hash value. If
     an update applied on R1 or R2, a CRC32 collision may occur. In such
     cases, finding a matching CRC32 in the hash table does not guarantee
     that the correct row is being updated. Therefore the algorithm
     iterates over multiple entries having the same CRC32, and compares
     the full record for each of them. However, the logic to exit this
     loop was incorrect.
    
     Consider an update on R2, in Rows_log_event::do_scan_and_update,
     during the first iteration of the inner do..while loop, no hash table
     entry is found to apply the current event related to R2. However,
     table->record[0] still refers to R1. Instead of exiting the inner
     do..while loop and calling scan_next_record, the while condition
     incorrectly assumes that table->record[0] has been updated to R2,
     causing R1 to be incorrectly updated. This leads to subsequent
     update/delete operation on R1/R2 failing.
    
     Fix:
     ----
     Modify Rows_log_event::do_scan_and_update to exit the inner do..while
     loop if no corresponding hash table entry is found for applying
     the current event.
    
     This patch is based on the contribution from Xiaodong Huang.
     We thank you for contributing to MySQL.
    
    Change-Id: Iecab0cb5c93c0b2a6feb12ba38596697509d4630

--- sql/log_event.cc
+++ sql/log_event.cc
@@ -9455,7 +9455,8 @@ int Rows_log_event::do_scan_and_update(Relay_log_info const *rli) {
           }
         } while (this->get_general_type_code() ==
                      binary_log::UPDATE_ROWS_EVENT &&
-                 !is_pk_present && (entry = m_hash.get(table, &m_local_cols)));
+                 !is_pk_present && entry &&
+                 (entry = m_hash.get(table, &m_local_cols)));
       } break;
 
       case HA_ERR_RECORD_DELETED:
